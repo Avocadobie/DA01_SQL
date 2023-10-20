72f0c9fc765571c3a37297e8e4ddc6a4769a8711
Avocadobie <147529192+Avocadobie@users.noreply.github.com>
2023-10-20 17:20:17 +0700

    Create buoi 9_baitap.sql

---
+++ buoi 9_baitap.sql
@@ -0,0 +1,42 @@
+---EX1
+SELECT
+ SUM(CASE
+   WHEN DEVICE_TYPE = 'tablet' OR DEVICE_TYPE = 'phone' 
+   THEN 1 ELSE 0 END) AS MOBILE_VIEWS,
+ SUM(CASE
+   WHEN DEVICE_TYPE = 'laptop'
+   THEN 1 ELSE 0 END) AS LAPTOP_VIEWS
+FROM VIEWERSHIP;
+
+---EX2
+SELECT *,
+CASE 
+    WHEN x+y > z and x + z > y and y + z > x then 'Yes'
+    ELSE 'No'
+END as triangle
+FROM triangle;
+
+---EX3
+
+---EX4
+elect name 
+from customer
+where referee_id not like 2
+or referee_id is null ;
+
+---EX5
+select survived, 
+SUM(CASE
+    WHEN pclass = 1 then 1
+    ELSE 0
+END) as first_class,
+SUM(CASE
+    WHEN pclass = 2 then 1
+    ELSE 0
+END) as second_class,
+SUM(CASE
+    WHEN pclass = 3 then 1
+    ELSE 0
+END) as third_class
+from titanic
+group by survived;
